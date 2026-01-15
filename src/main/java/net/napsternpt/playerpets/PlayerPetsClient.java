b67ba02486e0ffa84b15f3ab89e9fe61608250f8
NapsterNPT <188631398+napsternpt@users.noreply.github.com>
2026-01-15 23:03:13 +0100

    Initial commit

---
+++ src/main/java/net/napsternpt/playerpets/PlayerPetsClient.java
@@ -0,0 +1,10 @@
+package net.napsternpt.playerpets;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlayerPetsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/napsternpt/playerpets/PlayerPetsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.napsternpt.playerpets;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PlayerPetsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
